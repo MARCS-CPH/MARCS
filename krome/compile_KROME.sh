a602f81e4ff27ddd6da97fd8e8851a267179ed46
ThorstenBalduin <78664948+ThorstenBalduin@users.noreply.github.com>
2025-01-28 11:22:54 +0100

    Update compile_KROME.sh to better handle MARCS_build and added comments

--- krome/compile_KROME.sh
+++ krome/compile_KROME.sh
@@ -2,17 +2,22 @@ network=networks/non_eq/react_Chapman_photo_krome
 project=react_Chapman_photo_krome
 options=options_photochem
 
-if [ -d build_$project ] ; then
+if [ -d build_$project ] ; then #delete old build folder
 rm build_$project/*
 fi 
 
 ./krome -n $network -project=$project -options=$options
 
-if [ ! -d MARCS_build ] ; then
+if [ -d MARCS_build ] ; then #delete old MARCS_build
+echo "Deleting old MARCS_build folder"
+rm -r MARCS_build
+fi
+
+if [ ! -d MARCS_build ] ; then #create new MARCS_build folder if none exists
+echo "Creating new MARCS_build folder"
 mkdir MARCS_build
 fi
 
-cp build_$project/* MARCS_build
-cp build_$project/* build
+cp build_$project/* MARCS_build #copy over files from the krome build folder
 mv $project.kpj MARCS_build
-cp MARCS_build/reactions_verbatim.dat ../
+echo "Copy over files to MARCS_build"
